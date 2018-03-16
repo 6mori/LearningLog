2add149bb1e39a52e677dca8aa70cb6d308a0c0d
6mori <luhs1043@gmail.com>
2018-03-16 22:12:25 +0800

    Change static dir path

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -148,7 +147,7 @@ if os.getcwd() == '/app':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
