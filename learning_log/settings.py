d4fb1b228c574ca42cc66d81a78c5ab64408e506
6mori <luhs1043@gmail.com>
2018-03-17 09:09:58 +0800

    Add static url in settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -147,7 +147,8 @@ if os.environ['HOME'] == '/app':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
